a2c9b54c3fc9125b0e485c3b6d8320899c1cd26a
cykoravish <ravishbisht85@gmail.com>
2025-06-13 10:00:03 +0530

    remove unused react import

--- frontend/src/components/adpopup/AdPopup.tsx
+++ frontend/src/components/adpopup/AdPopup.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { X } from 'lucide-react';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
