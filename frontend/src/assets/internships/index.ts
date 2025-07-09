f3f3b8409729fb04f4bc5e7c6620efb3302d2ed3
cykoravish <ravishbisht85@gmail.com>
2025-07-09 17:44:02 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/assets/internships/index.ts
+++ frontend/src/assets/internships/index.ts
@@ -19,6 +19,33 @@ import intern17 from "./intern17.png";
 import intern18 from "./intern18.png";
 import intern19 from "./intern19.png";
 import intern20 from "./intern20.png";
+import intern21 from "./intern21.png";
+import intern22 from "./intern22.png";
+import intern23 from "./intern23.png";
+import intern24 from "./intern24.png";
+import intern25 from "./intern25.png";
+import intern26 from "./intern26.png";
+import intern27 from "./intern27.png";
+import intern28 from "./intern28.png";
+import intern29 from "./intern29.png";
+import intern30 from "./intern30.png";
+import intern31 from "./intern31.png";
+import intern32 from "./intern32.png";
+import intern33 from "./intern33.png";
+import intern34 from "./intern34.png";
+import intern35 from "./intern35.png";
+import intern36 from "./intern36.png";
+import intern37 from "./intern37.png";
+import intern38 from "./intern38.png";
+import intern39 from "./intern39.png";
+import intern40 from "./intern40.png";
+import intern41 from "./intern41.png";
+import intern42 from "./intern42.png";
+import intern43 from "./intern43.png";
+import intern44 from "./intern44.png";
+import intern45 from "./intern45.png";
+import intern46 from "./intern46.png";
+import intern47 from "./intern47.png";
 
 // intern logos
 import intern1Logo1 from "./intern1-1.png";
@@ -65,8 +92,6 @@ import intern13Logo1 from "./intern13-1.png";
 import intern13Logo2 from "./intern13-2.png";
 import intern13Logo3 from "./intern13-3.png";
 
-
-
 import intern14Logo1 from "./intern14-1.png";
 import intern14Logo2 from "./intern14-2.png";
 import intern14Logo3 from "./intern14-3.png";
@@ -74,9 +99,6 @@ import intern14Logo3 from "./intern14-3.png";
 import intern15Logo1 from "./intern15-1.png";
 import intern15Logo2 from "./intern15-2.png";
 import intern15Logo3 from "./intern15-3.png";
-
-
-
 
 const internImages = [
   { internImage: intern1, logo0: [intern1Logo1, intern1Logo2, intern1Logo3] },
@@ -89,16 +111,142 @@ const internImages = [
   { internImage: intern8, logo7: [intern8Logo1, intern8Logo2, intern8Logo3] },
   { internImage: intern9, logo8: [intern9Logo1, intern9Logo2, intern9Logo3] },
   { internImage: intern10, logo9: [intern1Logo1, intern1Logo2, intern1Logo3] },
-  { internImage: intern11, logo10: [intern11Logo1, intern11Logo2, intern11Logo3] },
-  { internImage: intern12, logo11: [intern12Logo1, intern12Logo2, intern12Logo3] },
-  { internImage: intern13, logo12: [intern13Logo1, intern13Logo2, intern13Logo3] },
-  { internImage: intern14, logo13: [intern14Logo1, intern14Logo2, intern14Logo3] },
-  { internImage: intern15, logo14: [intern15Logo1, intern15Logo2, intern15Logo3] },
+  {
+    internImage: intern11,
+    logo10: [intern11Logo1, intern11Logo2, intern11Logo3],
+  },
+  {
+    internImage: intern12,
+    logo11: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern13,
+    logo12: [intern13Logo1, intern13Logo2, intern13Logo3],
+  },
+  {
+    internImage: intern14,
+    logo13: [intern14Logo1, intern14Logo2, intern14Logo3],
+  },
+  {
+    internImage: intern15,
+    logo14: [intern15Logo1, intern15Logo2, intern15Logo3],
+  },
   { internImage: intern16, logo15: [intern1Logo1, intern1Logo2, intern1Logo3] },
   { internImage: intern17, logo15: [intern1Logo1, intern1Logo2, intern1Logo3] },
   { internImage: intern18, logo15: [intern1Logo1, intern1Logo2, intern1Logo3] },
   { internImage: intern19, logo15: [intern8Logo1, intern8Logo2, intern8Logo3] },
-  { internImage: intern20, logo15: [intern12Logo1, intern12Logo2, intern12Logo3] },
+  {
+    internImage: intern20,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern21,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern22,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern23,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern24,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern25,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern26,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern27,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern28,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern29,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern30,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern31,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern32,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern33,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern34,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern35,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern36,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern37,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern38,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern39,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern40,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern41,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern42,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern43,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern44,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern45,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern46,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
+  {
+    internImage: intern47,
+    logo15: [intern12Logo1, intern12Logo2, intern12Logo3],
+  },
 ];
 
 export default internImages;
